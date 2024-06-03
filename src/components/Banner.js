df994f631ce081f1d89dc91b67a8b7d675335327
Adeniyi Richard Adedokun <Adedokunrichard2@gmail.com>
2024-06-03 08:28:11 -0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,11 +8,14 @@ function Banner(props) {
       <div className="banner-content">
         <h2>{props.title}</h2>
         <p>{props.text}</p>
-        <Button buttonType={props.buttonType}>{props.buttonText}</Button>
+        <div className="buttons">
+          {props.buttons.map((button, index) => (
+            <Button key={index} buttonType={button.type}>{button.text}</Button>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
 
 export default Banner;
-
